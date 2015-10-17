37138fa61bf5c111ffaa50c441b470487195a2ec
Jarred Capellman <jcapellman@hotmail.com>
2015-10-17 14:55:33 -0400

    Added: Initial Checkin

---
+++ jcIQAPI/jcIQAPI.Tests.WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace jcIQAPI.Tests.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ jcIQAPI/jcIQAPI.Tests.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jcIQAPI.Tests.WebAPI.WebApiApplication" Language="C#" %>
